9d7f32ac0a8325e57323d9a2c20dfc8d4c03f91b
Blank21-xD <lama.safal21@gmail.com>
2026-04-27 23:06:56 +1000

    Displayed database items on the home page

--- my_project/views.py
+++ my_project/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from tracker.models import Item  # Import your model
 
 
 def home(request):
-    return render(request, 'home.html')
+    items = Item.objects.all()  # Fetch all items from the database
+    # Pass them to the HTML
+    return render(request, 'home.html', {'items': items})
