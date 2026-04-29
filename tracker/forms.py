5db1cecf9effe84f71cef60ab979e0cbf0560551
Blank21-xD <lama.safal21@gmail.com>
2026-04-29 22:47:26 +1000

    Applied Bootstrap styling to ItemForm and home layout

--- tracker/forms.py
+++ tracker/forms.py
@@ -6,3 +6,8 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['name', 'store', 'target_price']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Item Name'}),
+            'store': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. Woolworths'}),
+            'target_price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '0.00'}),
+        }
